fafd3e6ad1ce7bc8543d56174d143823b5e3ae9d
Berat Erdinc <beraterdinc42@gmail.com>
2021-12-31 10:24:47 +0300

    C# FORM

---
+++ HafızaOyunu/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            this.Hide();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Oyun yanda çıkan rastgele sayıları doğru yazarak sonuça ulaşma oyunudur");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
